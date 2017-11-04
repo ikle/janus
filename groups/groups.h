f8303b285837c883792a38cba0e4490c318d458b
Alexei A. Smekalkine <ikle@ikle.ru>
2017-11-04 22:01:35 +0300

    groups: move node definitions to separate object

--- groups/groups.h
+++ groups/groups.h
@@ -11,33 +11,8 @@
 
 #include <stdio.h>
 
-#include "address.h"
-#include "callout.h"
+#include "node.h"
 #include "seq.h"
-
-enum node_type {
-	NODE_STATIC,
-	NODE_DOMAIN,
-	NODE_ZONE,
-};
-
-struct node {
-	struct node *next;
-	enum node_type type;
-	struct address_seq seq;
-	char *name;
-	time_t ttl;
-	struct callout callout;
-};
-
-struct node *node_alloc_static (FILE *from);
-struct node *node_alloc_domain (const char *name);
-struct node *node_alloc_zone (const char *name);
-void node_free_schedule (struct node *o);
-
-void node_update (struct node *o, struct address_seq *seq);
-
-SEQ_DECLARE (node)
 
 struct group {
 	char *name;
