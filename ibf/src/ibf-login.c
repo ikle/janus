12067b714060eb34510553fa16852dc53f37e073
Alexei A. Smekalkine <ikle@ikle.ru>
2018-03-21 09:31:27 +0300

    ibf-login: embed avatar image into CGI application

--- ibf/src/ibf-login.c
+++ ibf/src/ibf-login.c
@@ -92,6 +92,18 @@ static void add_groups (const char *user, const char *addr)
 
 /* CGI application */
 
+extern const char _binary_avatar_png_start[];
+extern const char _binary_avatar_png_size;
+
+static void show_avatar (cgi_req *o)
+{
+	cgi_puts ("Status: 200 OK\r\n"
+		  "Content-Type: image/png\r\n\r\n", o);
+
+	cgi_write (_binary_avatar_png_start,
+		   (size_t) &_binary_avatar_png_size, o);
+}
+
 extern const char _binary_login_head_start[], _binary_login_tail_start[];
 extern const char _binary_login_head_size,    _binary_login_tail_size;
 
@@ -114,7 +126,7 @@ static void show_login (cgi_req *c, const char *ref)
 		   (size_t) &_binary_login_tail_size, c);
 }
 
-static void process (cgi_req *o)
+static void do_login (cgi_req *o)
 {
 	char buf[1024];
 	char **list = cgi_load_vars (o, buf, sizeof (buf));
@@ -132,6 +144,19 @@ static void process (cgi_req *o)
 		show_login (o, ref);
 
 	cgi_free_vars (list);
+}
+
+static void process (cgi_req *o)
+{
+	const char *path = cgi_getvar (o->envp, "SCRIPT_NAME");
+
+	if (strcmp (path, "/avatar.png") == 0)
+		show_avatar (o);
+	else if (strcmp (path, "/login") == 0)
+		do_login (o);
+	else
+		cgi_puts ("Status: 404 Not Found\r\n\r\n", o);
+
 }
 
 int main (int argc, char *argv[])
